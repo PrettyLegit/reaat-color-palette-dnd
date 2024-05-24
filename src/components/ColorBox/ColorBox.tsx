f843dbe20a30fa707b992fea83a8a274f9cf7a56
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-05-23 22:27:10 -0400

    add copy overlay animation

--- src/components/ColorBox/ColorBox.tsx
+++ src/components/ColorBox/ColorBox.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "./ColorBox.scss";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 export default function ColorBox({
   name,
@@ -9,8 +9,12 @@ export default function ColorBox({
   name: string;
   background: string;
 }) {
+  const [isCopied, setIsCopied] = useState(false);
+
   const handleCopy = useCallback(() => {
     navigator.clipboard.writeText(background);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 1500);
   }, [background]);
 
   return (
@@ -19,18 +23,21 @@ export default function ColorBox({
       className="color-box"
       style={{ backgroundColor: background }}
     >
-      <div className="copy-overlay" />
+      <div
+        style={{ backgroundColor: background }}
+        className={`copy-overlay ${isCopied && "show"}`}
+      />
+      <div className={`copy-msg ${isCopied && "show"}`}>
+        <h1>Copied!</h1>
+        <p>{background}</p>
+      </div>
       <div>
         <div className="box-content">
           <span>{name}</span>
         </div>
-        <button className="copy-button">
-          Copy
-        </button>
+        <button className="copy-button">Copy</button>
       </div>
-      <span className="see-more">
-        More
-      </span>
+      <span className="see-more">More</span>
     </div>
   );
 }
