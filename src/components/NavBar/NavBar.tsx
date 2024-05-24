57139cf827000eb20478a7c37d8111fbb1ea0016
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-05-24 08:14:57 -0400

    add controlled select via mui

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,30 @@
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css"; // import first
+import { useState } from "react";
 import "./NavBar.scss";
 
-import Slider from "rc-slider";
 function NavBar({
   level,
   changeLevel,
+  changeFormat,
 }: {
   level: number;
   changeLevel: (level: number | number[]) => void;
+  changeFormat: (
+    value: string,
+  ) => void;
 }) {
+  const [format, setFormat] = useState("hex" as string);
+
+  function handleChange(
+    event: SelectChangeEvent<string>,
+  ): void {
+    setFormat(event.target.value);
+    changeFormat(event.target.value);
+  }
+
   return (
     <header className="navbar">
       <div className="logo">
@@ -25,6 +41,17 @@ function NavBar({
           step={100}
           onChange={changeLevel}
         />
+      </div>
+      <div className="select-container">
+        <Select
+          //   native
+          value={format}
+          onChange={handleChange}
+        >
+          <MenuItem value="hex">HEX - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1.0)</MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/components/Palette/Palette.tsx
+++ src/components/Palette/Palette.tsx
@@ -1,6 +1,6 @@
 "use client";
-import "./Palette.scss";
 import ColorBox from "../ColorBox/ColorBox";
+import "./Palette.scss";
 
 import { useState } from "react";
 import NavBar from "../NavBar/NavBar";
@@ -28,18 +28,30 @@ type Colors = {
 export default function Palette({ palette }: PaletteProps) {
   const colorsArr = palette.colors;
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState("hex" as string);
 
   function changeLevel(level: number | number[]): void {
     setLevel(typeof level === "number" ? level : level[0]);
   }
 
+  function changeFormat(value: string): void {
+    setFormat(value);
+  }
+
   return (
     <div className="palette">
-      <NavBar level={level} changeLevel={changeLevel} />
-
+      <NavBar
+        level={level}
+        changeLevel={changeLevel}
+        changeFormat={changeFormat}
+      />
       <div className="palette-colors">
         {colorsArr[level].map((color) => (
-          <ColorBox key={color.name} name={color.name} background={color.hex} />
+          <ColorBox
+            key={color.name}
+            name={color.name}
+            background={color[format as keyof Color]}
+          />
         ))}
       </div>
     </div>
